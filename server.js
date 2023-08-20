5a2c1ddf84c6f828eb31e64b9959b032674a49e9
pracheth thamankar <pracheth.thamankar@gmail.com>
2023-08-20 20:43:55 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  res.status(200);
+  return res.send("Hello world!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  res.status(200);
+  res.json({ result: req.body.first + req.body.second });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  res.status(200);
+  res.json({ result: req.body.first - req.body.second });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
